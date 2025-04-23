3651f2bf8e5c82d50a3e6424239e1a293cb682f4
AwetTeame <awettmt@gmail.com>
2025-04-23 10:05:43 -0400

    get book details based on isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,9 +18,11 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    //Get book by isbn
+    const isbn = req.params.isbn
+    const book = books[isbn]
+    res.send(book)
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
