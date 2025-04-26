e25452e95af300c5460cd35bb77916358d2be1e2
AwetTeame <awettmt@gmail.com>
2025-04-26 00:39:59 -0400

    End point for getting book based on author parameter implemented.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,8 +26,21 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author
+    const bookKeys = Object.keys(books)
+    let matchingBooks = []
+
+    for(let key of bookKeys) {
+        if(books[key].author == author) {
+            matchingBooks.push(books[key])
+        }
+    }
+
+    if (matchingBooks.length > 0) {
+        res.status(200).json({ booksByAuthor: matchingBooks });
+    } else {
+        res.status(404).json({ message: "No books found for this author." });
+    }
 });
 
 // Get all books based on title
