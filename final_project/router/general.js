9efe2269289f44fae994c4373311dbf9cfd6606e
AwetTeame <awettmt@gmail.com>
2025-05-01 20:00:44 -0400

    Updating the get book detail by isbn, author, and title by adding asyc await call back to the endpoints.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -37,24 +37,26 @@ public_users.get('/',async function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',async function (req, res) {
     //Get book by isbn
-    const isbn = req.params.isbn
-    const book = books[isbn]
-    res.send(book)
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    await delay(6000);
+    res.send(book);
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author
-    const bookKeys = Object.keys(books)
-    let matchingBooks = []
+public_users.get('/author/:author',async function (req, res) {
+    const author = req.params.author;
+    const bookKeys = Object.keys(books);
+    let matchingBooks = [];
 
+    await delay(6000);
     for(let key of bookKeys) {
         if(books[key].author == author) {
             matchingBooks.push(books[key])
-        }
-    }
+        };
+    };
 
     if (matchingBooks.length > 0) {
         res.status(200).json({ booksByAuthor: matchingBooks });
@@ -64,22 +66,24 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title
-    const bookKeys = Object.keys(books)
-    let matchingBooks = []
+public_users.get('/title/:title',async function (req, res) {
+    const title = req.params.title;
+    const bookKeys = Object.keys(books);
+    let matchingBooks = [];
+
+    await delay(6000);
 
     for(let key of books) {
         if(books[key].title == title) {
             matchingBooks.push(books[key])
-        }
-    }
+        };
+    };
 
     if(matchingBooks.length > 0) {
         res.status(200).json({ booksByTitle: matchingBooks })
     } else {
         res.status(404).json({ message: "No books found for this author." })
-    }
+    };
 });
 
 //  Get book review
